99fb84f20ec90a288e88bb18c9f9ec83148eb7be
unknown <aakanksha.namrata@gmail.com>
2022-03-11 12:20:50 +0530

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/heroku'));
+app.use(express.static('./dist/myApp'));
 
 app.get('/*', function(req,res) {
     
@@ -13,4 +13,4 @@ res.sendFile('index.html');
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
