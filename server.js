80dcea2aefbfbc01d67c68455518334854c689d2
unknown <aakanksha.namrata@gmail.com>
2022-03-11 13:17:36 +0530

    commit

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static('./dist/myapp'));
-app.use(express.static(path.join(__dirname, 'dist','app_name')));
+app.use(express.static(path.join(__dirname, 'dist','my-app')));
 // res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
+res.sendFile(path.join(__dirname,'dist','my-app','index.html'));
 });
 
 // Start the app by listening on the default Heroku port
