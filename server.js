866c4f8a107915fd4224d76b0d98a628c6983d4b
unknown <aakanksha.namrata@gmail.com>
2022-03-11 13:25:03 +0530

    commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'dist','my-app')));
 // res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'dist','my-app','index.html'));
+res.sendFile(path.join(__dirname,'dist','my-app','/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
