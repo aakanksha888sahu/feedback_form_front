4a7f7bc559fd59e92f419921a8356b4966d5ec84
unknown <aakanksha.namrata@gmail.com>
2022-03-11 12:50:45 +0530

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/myApp'));
+app.use(express.static('./dist/myapp'));
 
 app.get('/*', function(req,res) {
     
